7aca0bd0b1da08cc24dffdad4eb755ad17991114
Kevin Musiorski <kmusio@artic.edu>
2018-05-09 14:36:03 -0500

    * adds devise's basic auth into application controller disables * authenticity token protection for demo since I fudged the logic up at some point today * started a spec, but then said hey, just try it!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,22 @@ class ApplicationController < ActionController::Base
   include Hyrax::ThemedLayoutController
   with_themed_layout '1_column'
 
-  protect_from_forgery with: :exception
+  before_action :check_auth_with_http_basic, if: :http_auth_header?
+
+  # protect_from_forgery with: :exception, unless: :http_auth_header?
+  # protect_from_forgery with: :null_session, if: :http_auth_header?
+  # protect_from_forgery with: :null_session
+
+  def check_auth_with_http_basic
+    authenticate_or_request_with_http_basic do |username,password|
+      resource = User.find_by_email(username)
+      if resource.valid_password?(password)
+        sign_in :user, resource
+      end
+    end
+  end
+
+  def http_auth_header?
+    request.headers["HTTP_AUTHORIZATION"].present?
+  end
 end
---
+++ spec/controllers/hyrax/filesets_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Hyrax::FileSetsController do
+  # it "should return a 403 if no credentials are provided" do
+  #   expect(response).to have_http_status 403
+  #   byebug
+  #   patch :udpate
+  # end
+end
